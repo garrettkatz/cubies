66f0c20d66411a43153cd0306382ecc0e2677b0b
garrettkatz <garrett.katz@gmail.com>
2021-07-29 12:24:18 -0400

    tested run algorithm with two patterns

--- algorithm.py
+++ algorithm.py
@@ -24,7 +24,7 @@ def macro_search(state, domain, bfs_tree, pattern_database, max_depth):
     # Failure if no path to macro found
     return False
 
-def run(state, domain, pattern_database, max_depth, max_macros):
+def run(state, domain, bfs_tree, pattern_database, max_depth, max_macros):
     # returns plan = [...,(actions, sym index, macro),...] a sequence of macro_search results to the solved state
     # or plan = False if no plan is found
     
@@ -33,7 +33,7 @@ def run(state, domain, pattern_database, max_depth, max_macros):
     for num_macros in range(max_macros):
 
         # Search for next macro
-        result = macro_search(state, domain, pattern_database, max_depth)
+        result = macro_search(state, domain, bfs_tree, pattern_database, max_depth)
         
         # Return failure if none found
         if result is False: return False
@@ -54,6 +54,34 @@ def run(state, domain, pattern_database, max_depth, max_macros):
 
 if __name__ == "__main__":
 
+    # #### test macro_search
+    # max_depth = 2
+
+    # from cube import CubeDomain
+    # domain = CubeDomain(3)
+
+    # from tree import SearchTree
+    # bfs_tree = SearchTree(domain, max_depth)
+
+    # from pattern_database import PatternDatabase
+    # # patterns = domain.solved_state().reshape(1,-1)
+    # # macros = [((0,0,0),)]
+    # patterns = domain.perform((0,0,1), domain.solved_state()).reshape(1,-1)
+    # macros = [((0,0,3),)]
+    # pattern_database = PatternDatabase(patterns, macros)
+
+    # state = domain.solved_state()
+    # state = domain.perform((0,0,1),state)
+    # state = domain.symmetries_of(state)[2,:]
+    # state = domain.perform((1,0,1),state)
+    # result = macro_search(state, domain, bfs_tree, pattern_database, max_depth)
+    # if result != False:
+    #     path, s, macro = result
+    #     print(path)
+    #     print(s)
+    #     print(macro)
+
+    #### test run
     max_depth = 2
 
     from cube import CubeDomain
@@ -62,21 +90,49 @@ if __name__ == "__main__":
     from tree import SearchTree
     bfs_tree = SearchTree(domain, max_depth)
 
+    import numpy as np
     from pattern_database import PatternDatabase
-    # patterns = domain.solved_state().reshape(1,-1)
-    # macros = [((0,0,0),)]
-    patterns = domain.perform((0,0,1), domain.solved_state()).reshape(1,-1)
-    macros = [((0,0,3),)]
+    state = domain.solved_state()
+    patterns = np.stack((
+        domain.execute([(0,0,1),(1,0,1)], state),
+        domain.execute([(0,0,1),(1,0,1),(2,0,1),(1,0,1),(0,0,1)], state),
+    ))
+    macros = (
+        [(1,0,3),(0,0,3)],
+        [(0,0,3),(1,0,3),(2,0,3)],
+    )
     pattern_database = PatternDatabase(patterns, macros)
 
-    state = domain.solved_state()
-    state = domain.perform((0,0,1),state)
-    state = domain.symmetries_of(state)[2,:]
-    state = domain.perform((1,0,1),state)
-    result = macro_search(state, domain, bfs_tree, pattern_database, max_depth)
+    state = domain.perform((1,0,1), patterns[1])
+    result = run(state, domain, bfs_tree, pattern_database, max_depth=1, max_macros=2)
     if result != False:
-        path, s, macro = result
-        print(path)
-        print(s)
-        print(macro)
+
+        import matplotlib.pyplot as pt
+        def draw(st, title, i):
+            ax = pt.subplot(4, 6, i)
+            domain.render(st, ax, 0, 0)
+            ax.axis("equal")
+            ax.axis('off')
+            ax.set_title(title)
+
+        i = 1
+        draw(state, "initial", i)
+        for (actions, sym, macro) in result:
+            print(actions)
+            print(sym)
+            print(macro)
+
+            for action in actions:
+                state = domain.perform(action, state)
+                draw(state, str(action), i)
+                i += 1
+            state = domain.symmetries_of(state)[sym]
+            draw(state, str(sym), i)
+            i += 1
+            for action in macro:
+                state = domain.perform(action, state)
+                draw(state, str(action), i)
+                i += 1
+
+        pt.show()
 
