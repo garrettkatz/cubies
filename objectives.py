c1ca066be370e82c79951870c4ad98c83759f42f
garrettkatz <garrett.katz@gmail.com>
2021-08-01 15:09:42 -0400

    initial version of training

--- objectives.py
+++ objectives.py
@@ -6,19 +6,50 @@ smaller macro lengths
 less complex patterns (like colors grouped together, more zeros, fewer variables)
 """
 
+from algorithm import run
+
+def evaluate(domain, bfs_tree, pattern_database, rng, num_instances, max_scrambles, max_depth, max_macros):
+
+    result = {}
+    scramble_length = {}
+    plan_length = {}
+    for i in range(num_instances):
+        scramble_length[i] = rng.integers(1, max_scrambles, endpoint=True)
+        state = domain.random_state(scramble_length[i], rng)
+        result[i] = run(state, domain, bfs_tree, pattern_database, max_depth, max_macros)
+        
+        if result[i] != False:
+            plan_length[i] = 0
+            for (actions, sym, macro) in result[i]: plan_length[i] += len(actions) + len(macro)
+
+    # smaller number of patterns
+    pattern_size = pattern_database.patterns.shape[0] // 24
+
+    # smaller macro lengths
+    macro_size = sum(map(len, pattern_database.macros)) / 24
+
+    # TODO: less complex patterns (like colors grouped together/symmetric, more zeros, fewer variables)
+
+    # solve in <= min 20, scramble_length steps
+    godly_solves = sum(int((result[i] != False) and (plan_length[i] <= min(20, scramble_length[i]))) for i in range(num_instances))
+
+    return pattern_size, macro_size, godly_solves
+
 if __name__ == "__main__":
     
     cube_size = 3
-    max_scramble_length = 5
+    max_scrambles = 5
     num_instances = 128
-    max_depth = 3
+    tree_depth = 3
+    max_depth = 1
+    max_macros = 2
     num_initial_patterns = 32
 
     from cube import CubeDomain
     domain = CubeDomain(cube_size)
 
     from tree import SearchTree
-    bfs_tree = SearchTree(domain, max_depth)
+    bfs_tree = SearchTree(domain, tree_depth)
 
     import numpy as np
     rng = np.random.default_rng()
@@ -34,39 +65,12 @@ if __name__ == "__main__":
         rng.choice(valid_actions, size = rng.integers(1,4), replace=True)
         for _ in range(num_initial_patterns)]
     pattern_database = PatternDatabase(patterns, macros, domain)
-    
-    from algorithm import run
-    
-    result = {}
-    scramble_length = {}
-    plan_length = {}
-    for i in range(num_instances):
-        scramble_length[i] = rng.integers(1, max_scramble_length, endpoint=True)
-        state = domain.random_state(scramble_length[i], rng)
-        result[i] = run(state, domain, bfs_tree, pattern_database, max_depth=1, max_macros=2)
-        print(i, result[i])
-        
-        if result[i] != False:
-            plan_length[i] = 0
-            for (actions, sym, macro) in result[i]: plan_length[i] += len(actions) + len(macro)
 
-    # smaller number of patterns
-    num_patterns = pattern_database.patterns.shape[0] / 24
-
-    # smaller macro lengths
-    macro_size = sum(map(len, pattern_database.macros)) / 24
-
-    # TODO: less complex patterns (like colors grouped together/symmetric, more zeros, fewer variables)
-
-    # solve in <= 20 steps
-    godly_solves = sum(int((result[i] != False) and (plan_length[i] <= 20)) for i in range(num_instances))
-    
-    # solve in <= scramble_length steps
-    scramble_solves = sum(int((result[i] != False) and (plan_length[i] <= scramble_length[i])) for i in range(num_instances))
+    objectives = evaluate(domain, bfs_tree, pattern_database, rng, num_instances, max_scrambles, max_depth, max_macros)
+    pattern_size, macro_size, godly_solves = objectives
 
     print()
     print("num_patterns = %d" % num_patterns)
     print("macro_size = %d" % macro_size)
     print("godly_solves = %d" % godly_solves)
-    print("scramble_solves = %d" % scramble_solves)
 
--- train.py
+++ train.py
@@ -1,24 +1,89 @@
+from objectives import evaluate
 
 if __name__ == "__main__":
     
+    cube_size = 3
+    max_scrambles = 5
+    num_instances = 32
+    tree_depth = 3
+    max_depth = 1
+    max_macros = 2
+    num_patterns = 32
+    unnormalized_wildcard_weight = 2
+    population_size = 64
+    num_generations = 64
+    dotrain = True
+
     from cube import CubeDomain
-    domain = CubeDomain(3)
-    scramble_length = 100
+    domain = CubeDomain(cube_size)
+    solved = domain.solved_state()
+    valid_actions = list(domain.valid_actions(solved))
+
+    from tree import SearchTree
+    bfs_tree = SearchTree(domain, tree_depth)
+
+    import numpy as np
+    rng = np.random.default_rng()
+    probs = np.ones(7)
+    probs[0] = unnormalized_wildcard_weight
+    probs /= probs.sum()
+
+    from pattern_database import PatternDatabase
+    best_patterns = rng.choice(np.arange(7), size = (num_patterns, solved.size), p = probs)
+    best_macros = [
+        rng.choice(valid_actions, size = rng.integers(1,4), replace=True)
+        for _ in range(num_patterns)]
+    best_solves = 0
+
+    import pickle as pk
     
-    import random
-    states = [domain.solved_state()]
-    actions = []
-    for s in range(scramble_length):
-        action = random.choice(list(domain.valid_actions(states[s])))
-        state = domain.perform(action, states[s])
-        actions.append(action)
-        states.append(state)
+    if dotrain:
+
+        all_patterns = {}
+        all_macros = {}
+        all_godly_solves = {}
+        for gen in range(num_generations):
+        
+            all_patterns[gen] = {}
+            all_macros[gen] = {}
+            all_godly_solves[gen] = {}
+            for rep in range(population_size):
+    
+                patterns = best_patterns.copy()
+                macros = [list(macro) for macro in best_macros]
+                for m in range(len(macros)):
+                    mutators = np.flatnonzero(rng.random(domain.state_size()) < 0.05)
+                    patterns[m, mutators] = rng.choice(np.arange(7), size = mutators.size, p = probs)
+                    mutators = np.flatnonzero(rng.random(len(macros[m])) < 0.1)
+                    for a in mutators: macros[m][a] = rng.choice(valid_actions)
+                
+                pattern_database = PatternDatabase(patterns, macros, domain)
+    
+                objectives = evaluate(domain, bfs_tree, pattern_database, rng, num_instances, max_scrambles, max_depth, max_macros)
+                pattern_size, macro_size, godly_solves = objectives
+                
+                all_patterns[gen][rep] = patterns
+                all_macros[gen][rep] = macros
+                all_godly_solves[gen][rep] = godly_solves
+                with open("data.pkl", "wb") as f: pk.dump((all_patterns, all_macros, all_godly_solves), f)
+    
+                print("  rep %d: %d godly solves" % (rep, godly_solves))
+            
+            best = np.argmax([all_godly_solves[gen][rep] for rep in range(population_size)])
+            if all_godly_solves[gen][best] > best_solves:
+                best_solves = all_godly_solves[gen][best]
+                best_patterns = all_patterns[gen][best]
+                best_macros = all_macros[gen][best]
+            print("gen %d: best godly solves = %d" % (gen, best_solves))
+
+    with open("data.pkl", "rb") as f: (patterns, macros, godly_solves) = pk.load(f)
     
     import matplotlib.pyplot as pt
-    for s, state in enumerate(states):
-        ax = pt.subplot(10, 11, s+1)
-        domain.render(state, ax, 0, 0)
-        ax.axis("equal")
-        ax.axis('off')
+    pt.subplot(1,2,1)
+    pt.hist(godly_solves[0].values())
+    pt.hist(godly_solves[len(godly_solves)-1].values())
+    pt.subplot(1,2,2)
+    pt.plot([max(godly_solves[gen].values()) for gen in range(len(godly_solves))])
     pt.show()
+
 
